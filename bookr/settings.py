3eea9baf705b130482ae6b4fd10337357cb6d182
Yunaluman <110559500+Break-Carrier@users.noreply.github.com>
2026-03-19 15:06:11 +0100

    Update database configuration in settings.py to use PostgreSQL (#1)
    - Changed database engine from SQLite to PostgreSQL
    - Updated database name, user, password, host, and port settings
    
    This change facilitates a more robust development environment.

--- bookr/settings.py
+++ bookr/settings.py
@@ -74,10 +74,23 @@ WSGI_APPLICATION = "bookr.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# Configuration SQLite (pour développement rapide)
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
+#    }
+#}
+
+# Configuration PostgreSQL (décommenter pour utiliser PostgreSQL)
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "bookr_db",
+        "USER": "postgres",
+        "PASSWORD": "appl32001",
+        "HOST": "localhost",
+        "PORT": "5433"
     }
 }
 
